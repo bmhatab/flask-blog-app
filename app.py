29f9c6f6c2131451a20bc89857556a9169919391
serendi <bmhatab@gmail.com>
2023-01-02 03:12:22 +0800

    added features

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,flash
+from flask import Flask, render_template,flash,request
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
@@ -94,7 +94,25 @@ def add_user():
     our_users = Users.query.order_by(Users.date_added)   
     return render_template('add_user.html',form=form,name=name,our_users=our_users)
 
+#update database
+@app.route('/update/<int:id>', methods =['GET','POST'])
+def update(id):
+    form = UserForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update = request.form['name']
+        name_to_update = request.form['email']
+        try:
+            db.session.commit()
+            flash("User updated sucessfully")
+            return render_template("update.html",form=form,name_to_update=name_to_update)
+            
+        except:
+            flash("Error! Problem with entry")
+            return render_template("update.html",form=form,name_to_update=name_to_update)
 
+    else:
+         return render_template("update.html",form=form,name_to_update=name_to_update)
 #create error handler for pages
 #----------------------------------------------------------------------------------------------------
 #Invalid URL
