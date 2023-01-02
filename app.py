f0347638081373fa7eee3879be2dee29711a940d
serendi <bmhatab@gmail.com>
2023-01-03 04:34:26 +0800

    Added passsword hashing

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template,flash,request
 from flask_wtf import FlaskForm
-from wtforms import Form,StringField,SubmitField
-from wtforms.validators import DataRequired
+from wtforms import Form,StringField,SubmitField,PasswordField,ValidationError
+from wtforms.validators import DataRequired,EqualTo,Length
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 #create an instance of flask
 app = Flask(__name__)
@@ -26,6 +28,20 @@ class Users(db.Model):
     name = db.Column(db.String(200),nullable=False)
     email = db.Column(db.String(120),nullable=False, unique=True)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    # password work stuff
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Password does not have a read attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash, password)
+
 
     # Create a string
     def __repr__(self):
@@ -54,11 +70,16 @@ class UserForm(FlaskForm):
     name = StringField("Name : ", validators=[DataRequired()])
     email = StringField("Email : ", validators=[DataRequired()])
     submit = SubmitField("Submit")
+    password_hash = PasswordField("Password : ", validators=[DataRequired(),EqualTo('password_hash_v',message="Passwords must match!")])
+    password_hash_v = PasswordField("Confirm Password : ", validators=[DataRequired()])
 
 class UpdateForm(FlaskForm):
     name = StringField("Name : ", validators=[DataRequired()])
     email = StringField("Email : ", validators=[DataRequired()])
     submit = SubmitField("Submit")
+    password_hash = PasswordField("Password : ", validators=[DataRequired(),EqualTo('password_hash_v',message="Passwords must match!")])
+    password_hash_v = PasswordField("Confirm Password : ", validators=[DataRequired()])
+    
 
 
 
@@ -88,17 +109,21 @@ def name():
 
 @app.route('/user/add', methods =['GET','POST'])
 def add_user():
+    
     name = None
     form = UserForm()
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data,email=form.email.data)
+            # Hash password first
+            hash_pw = generate_password_hash(form.password_hash.data, "sha256")
+            user = Users(name=form.name.data,email=form.email.data,password_hash=hash_pw)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
+        form.password_hash.data = ''
         flash("User Added Sucessfully")
     #To display user names on the page 
     our_users = Users.query.order_by(Users.date_added)   
@@ -112,6 +137,7 @@ def update(id):
     if request.method == "POST" and form.validate():
         user.name = form.name.data
         user.email = form.email.data
+        user.password_hash=form.password_hash.data
         db.session.commit()
         flash("User Added Sucessfully")
         return render_template("update.html",form=form,user=user)
